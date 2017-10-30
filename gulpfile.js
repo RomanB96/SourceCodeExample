56244f6bf7dc98e11f23638e9316672d4224a995
Roman Baran <roman_96@outlook.it>
2017-10-30 12:44:46 +0100

    Added sassdoc (for small scss size)

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,26 @@ var gulp         = require('gulp');
 var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
+var sassdoc      = require('sassdoc');
 
 // paths
-var src        = './src';
-var dist       = './dist';
-var css_input  = src + '/css/**/*.scss';
-var css_output = dist + '/css';
-var css_maps   = css_output + '/maps';
-
+var src         = './src';
+var dist        = './dist';
+var css_input   = src + '/css/**/*.scss';
+var css_output  = dist + '/css';
+var css_maps    = css_output + '/maps';
+var css_maps    = css_output + '/maps';
+var css_sassdoc = css_output + '/sassdoc'; 
 
 // sass
 var sassOptions = {
     errLogToConsole: true,
     outputStyle: 'expanded'
 };
+var sassdocOptions = {
+    dest: css_sassdoc
+};
+
 
 gulp.task('default', function () {
     console.log(
@@ -34,5 +40,7 @@ gulp.task('sass', function () {
         .pipe(sass().on('error', sass.logError))
         .pipe(sourcemaps.write(css_maps))
         .pipe(autoprefixer())
-        .pipe(gulp.dest(css_output));
+        .pipe(gulp.dest(css_output))
+        .pipe(sassdoc(sassdocOptions))
+        .resume();
 });
