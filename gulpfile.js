c7c44f8fbf11736b1c001afb7434cba88a8e16a3
Roman Baran <roman_96@outlook.it>
2017-10-30 12:38:15 +0100

    Added autoprefixer + default task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 // node_modules
-var gulp       = require('gulp');
-var sass       = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps')
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
 
 // paths
 var src        = './src';
@@ -17,11 +18,21 @@ var sassOptions = {
     outputStyle: 'expanded'
 };
 
+gulp.task('default', function () {
+    console.log(
+        '\n\r' +
+        'SCRIPTS\n\r' +
+        'npm run build -> build sass' +
+        '\n\r'
+    );
+});
+
 gulp.task('sass', function () {
-  return gulp
-    .src(css_input)
-    .pipe(sourcemaps.init())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(sourcemaps.write(css_maps))
-    .pipe(gulp.dest(css_output));
+    return gulp
+        .src(css_input)
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write(css_maps))
+        .pipe(autoprefixer())
+        .pipe(gulp.dest(css_output));
 });
