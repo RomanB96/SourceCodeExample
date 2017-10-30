6087bf6c64255822e1839a3bc8ad6c5cb4180a9e
Roman Baran <roman_96@outlook.it>
2017-10-30 15:06:03 +0100

    Revert "Added style formatted on build"
    This reverts commit 1e4e9c20b1ad759e605d15bfe81d36c4677555e1.

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ var sass         = require('gulp-sass');
 var sassdoc      = require('sassdoc');
 var source       = require('vinyl-source-stream');
 var sourcemaps   = require('gulp-sourcemaps');
-var stylefmt     = require('gulp-stylefmt');
 var uglify       = require('gulp-uglify');
 
 
@@ -71,7 +70,6 @@ gulp.task('release', gulpsync.async(['css_release', 'js_release']));
 gulp.task('css_dev', function () {
     return gulp
         .src(css_input)
-        .pipe(stylefmt())
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         // .pipe(sourcemaps.write(css_maps))
