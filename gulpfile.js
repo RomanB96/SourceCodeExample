6a45ac82f5296bb55b157e1a505746388f467131
Roman Baran <roman_96@outlook.it>
2017-10-30 14:03:33 +0100

    Added basic babel

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,25 @@ var sass         = require('gulp-sass');
 var sourcemaps   = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var sassdoc      = require('sassdoc');
+var browserify   = require('browserify');
+var babelify     = require('babelify');
+var source       = require('vinyl-source-stream');
 
 // paths
-var src         = './src';
-var dist        = './dist';
-var release     = './release';
+var src     = './src';
+var dist    = './dist';
+var release = './release';
+// css
 var css_input   = src + '/css/**/*.scss';
 var css_output  = dist + '/css';
 var css_release = release + '/css';
 // var css_maps    = css_output + '/maps';  // Don't work with path
-var css_sassdoc = css_output + '/sassdoc'; 
+var css_sassdoc = css_output + '/sassdoc';
+// js
+var js_entry   = src + '/js/index.js';
+var js_input   = src + '/js/**/*.js';
+var js_output  = dist + '/js';
+var js_release = release + '/js';
 
 // sass
 var sassOptions = {
@@ -27,15 +36,10 @@ var sassdocOptions = {
 
 gulp.task('default', function () {
     console.log(
-        '\n\r' +
-        'SCRIPTS' +
-        '\n\r' +
-        'npm run build   -> build sass' +
-        '\n\r' +
-        'npm run watch   -> watch sass' +
-        '\n\r' +
-        'npm run release -> release sass' +
-        '\n\r'
+        '\n\r' + 'SCRIPTS' + '\n\r' +
+        'npm run build   -> build sass'   + '\n\r' +
+        'npm run watch   -> watch sass'   + '\n\r' +
+        'npm run release -> release sass' + '\n\r'
     );
 });
 
@@ -50,6 +54,14 @@ gulp.task('sass', function () {
         .pipe(gulp.dest(css_output))
         .pipe(sassdoc(sassdocOptions))
         .resume();
+});
+
+gulp.task('javascript', function () {
+    return browserify( { entries: js_entry, debug: true } )
+        .transform("babelify", { presets: ["es2015"] })
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(gulp.dest(js_output));
 });
 
 gulp.task('watch', function() {
