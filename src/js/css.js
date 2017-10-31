27d00473162db46e30906da6f611f9deed79aa14
RomanB96 <roman_96@outlook.it>
2017-10-31 18:20:11 +0100

    Added Roboto font

--- src/js/css.js
+++ src/js/css.js
@@ -3,20 +3,21 @@ class Css {
         this.cssLink;
     }
 
-    appendCss() {
+    appendCss(href) {
+        this.cssLink      = document.createElement('link');
+        this.cssLink.rel  = 'stylesheet';
+        this.cssLink.href = href;
+        this.cssLink.type = 'text/css';
+
         document
             .getElementsByTagName('head')[0]
             .appendChild(this.cssLink);
     }
 
     init() {
-        try {
-            this.cssLink = document.createElement('link');
-            this.cssLink.rel = 'stylesheet';
-            this.cssLink.href = './index.css';
-            this.cssLink.type = 'text/css';
-
-            this.appendCss();
+        try {            
+            this.appendCss('https://fonts.googleapis.com/css?family=Roboto:400,400i,700,700i');
+            this.appendCss('./index.css'); 
         } catch (e) {
             console.error(e, e.stack);
         }
