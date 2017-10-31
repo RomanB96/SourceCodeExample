783490f29ecafa3e7df11d64ac069232195c2cae
RomanB96 <roman_96@outlook.it>
2017-10-31 14:09:18 +0100

    First remork - arrow functions

--- gulpfile.js
+++ gulpfile.js
@@ -66,14 +66,14 @@ var transformPresetsOptions = {
 
 
 // Default tasks
-gulp.task('default', function () {
+gulp.task('default', () =>
     console.log(
         '\n\r' + 'SCRIPTS' + '\n\r' +
         'npm run build'    + '\n\r' +
         'npm run watch'    + '\n\r' +
         'npm run release'  + '\n\r'
-    );
-});
+    )
+);
 
 gulp.task('build', gulpsync.async(['html_dev', 'css_dev', 'js_dev', 'img_dev']));
 gulp.task('watch', gulpsync.sync(['build', 'watch_builded']));
@@ -82,30 +82,26 @@ gulp.task('release', gulpsync.async(['html_release', 'css_release', 'js_release'
 
 
 // HTML
-gulp.task('html_dev', function() {
-    return gulp
-        .src(html_input)
-        .pipe(gulp.dest(html_build));
-});
-gulp.task('html_watch', function() {
-    return gulp
-        .watch(html_input, ['html_dev'])
+gulp.task('html_dev', () =>
+    gulp.src(html_input)
+        .pipe(gulp.dest(html_build))
+);
+gulp.task('html_watch', () =>
+    gulp.watch(html_input, ['html_dev'])
         .on('change', function(event) {
             console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        });
-});
-gulp.task('html_release', function() {
-    return gulp
-        .src(html_input)
+        })
+);
+gulp.task('html_release', () =>
+    gulp.src(html_input)
         .pipe(htmlmin(htmlminOptions))
-        .pipe(gulp.dest(html_release));
-});
+        .pipe(gulp.dest(html_release))
+);
 
 
 // CSS
-gulp.task('css_dev', function () {
-    return gulp
-        .src(css_input)
+gulp.task('css_dev', () =>
+    gulp.src(css_input)
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         // .pipe(sourcemaps.write(css_maps))
@@ -113,60 +109,55 @@ gulp.task('css_dev', function () {
         .pipe(autoprefixer())
         .pipe(gulp.dest(css_build))
         .pipe(sassdoc(sassdocOptions))
-        .resume();
-});
-gulp.task('css_watch', function() {
-    return gulp
-        .watch(css_input, ['css_dev'])
+        .resume()
+);
+gulp.task('css_watch', () =>
+    gulp.watch(css_input, ['css_dev'])
         .on('change', function(event) {
             console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        });
-});
-gulp.task('css_release', function () {
-    return gulp
-        .src(css_input)
+        })
+);
+gulp.task('css_release', () =>
+    gulp.src(css_input)
         .pipe(sass(sassReleaseOptions))
         .pipe(autoprefixer())
-        .pipe(gulp.dest(css_release));
-});
+        .pipe(gulp.dest(css_release))
+);
 
 
 // JS
-gulp.task('js_dev', function () {
-    return browserify(browserifyOptions)
+gulp.task('js_dev', () =>
+    browserify(browserifyOptions)
         .transform("babelify", transformPresetsOptions)
         .bundle()
         .pipe(source('index.js'))
-        .pipe(gulp.dest(js_build));
-});
-gulp.task('js_watch', function () {
-    return gulp
-        .watch(js_input, ['js_dev'])
+        .pipe(gulp.dest(js_build))
+);
+gulp.task('js_watch', () =>
+    gulp.watch(js_input, ['js_dev'])
         .on('change', function(event) {
             console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-        });
-});
-gulp.task('js_release', function () {
-    return browserify(browserifyOptions)
+        })
+);
+gulp.task('js_release', () =>
+    browserify(browserifyOptions)
         .transform("babelify", transformPresetsOptions)
         .bundle()
         .pipe(source('index.js'))
         .pipe(buffer())
         .pipe(uglify())
-        .pipe(gulp.dest(js_release));
-});
+        .pipe(gulp.dest(js_release))
+);
 
 
 // IMG
-gulp.task('img_dev', function () {
-    return gulp
-        .src(img_input)
+gulp.task('img_dev', () =>
+    gulp.src(img_input)
         .pipe(imagemin())
         .pipe(gulp.dest(img_build))
-});
-gulp.task('img_release', function () {
-    return gulp
-        .src(img_input)
+);
+gulp.task('img_release', () =>
+    gulp.src(img_input)
         .pipe(imagemin())
         .pipe(gulp.dest(img_release))
-});
+);
